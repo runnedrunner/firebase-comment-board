b8024ef13b9a8bc6f1484012a9fa4ef53997281e
runnedrunner <auszhou@nuevaschool.org>
2016-11-23 08:35:31 -0800

    Classadoo autocommit

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 var commentInput = $("#comment-input")
-var commentDisplay = $(".not-actually-the-right-class")
+var commentDisplay = $(".comment-display")
 
 commentInput.keydown(function(e) {
   if (e.which == 13) {
@@ -7,6 +7,6 @@ commentInput.keydown(function(e) {
   }
 })
 
-onNewListItem("whats-my-name-again", function(newComment) {
-  commentDisplay.append("<span>" + newComment + "</span>")
+onNewListItem("comments", function(newComment) {
+  commentDisplay.append("<div>" + newComment + "</div>")
 })
